df8ab66d9f75471fe17285f4c727b841068f3d20
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2020-06-14 15:21:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/*realiza os cálculos criando 3 variáveis e retornando o valor de duas inputs, por fim, 
+atribuindo a 'x', o valor de 'a / (b * b)' e retornando esse resultado 'x' dentro da div 'mostrar'*/
+function calcular() {
+    var a, b, x;
+    a = document.getElementById('pesoKg').value;
+    b = document.getElementById('alturaM').value;
+    x = a / (b * b);
+    document.getElementById('mostrar').innerHTML= x.toFixed(2);
+
+    if(x >= 40) {
+        document.getElementById('situacao').innerHTML='Obesidade G3';
+    } else if(x < 40 && x > 35) {
+        document.getElementById('situacao').innerHTML='Obesidade G2';
+    } else if(x < 35 && x >= 30) {
+        document.getElementById('situacao').innerHTML='Obesidade G1';
+    } else if(x < 29.9 && x >= 25) {
+        document.getElementById('situacao').innerHTML='Est Sobrepeso';
+    } else if(x < 24.9 && x >= 18.5) {
+        document.getElementById('situacao').innerHTML='Peso  . Normal';
+    } else if(x < 18.5) {
+        document.getElementById('situacao').innerHTML='Estado. Magro';
+    }
+}
